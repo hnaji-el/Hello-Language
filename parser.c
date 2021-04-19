19b7a9d1362234c920c73af313bd1e7c42b808e6
hnaji-el <hamidnaji981@gmail.com>
2021-04-19 00:30:26 +0000

    PARSER version 2

--- parser.c
+++ parser.c
@@ -29,12 +29,88 @@ void		parser_expected_token(t_parser *parser, t_token_type type)
 	}
 }
 
+t_ast		*parser_parse_string(t_parser *parser)
+{
+	t_ast	*ast;
+
+	ast = init_ast(AST_STRING);
+	ast->string_value = parser->cur_token->value;
+	return (ast);
+}
+
+t_ast		*parser_parse_function_call(t_parser *parser)
+{
+	t_ast	*ast;
+	t_ast	*ast_argument;
+
+	if (strcmp(parser->prev_token->value, "print") == 0)
+		ast = builtin_print_function(parser)
+	ast = init_ast(AST_FUNCTION_CALL);
+	ast->compound_value = (t_ast **)malloc(sizeof(t_ast *));
+	ast_statement = parser_parse_statement(parser);
+	ast->compound_value[0] = ast_statement;
+	ast->compound_size += 1;
+	while (parser->cur_token->type == TOKEN_SEMI)
+	{
+		parser_expected_token(parser, TOKEN_SEMI);
+		ast->compound_size += 1;
+		ast->compound_value = realloc(
+				ast->compound_value,
+				ast->compound_size * sizeof(t_ast *)
+				);
+		ast_statement = parser_parse_statement(parser);
+		ast->compound_value[ast->compound_size - 1] = ast_statement;
+	}
+	return (ast);
+}
+
+t_ast		*parser_parse_variable(t_parser *parser)
+{
+	t_ast	*ast;
+	t_token	*token_value;
+
+	token_value = parser->cur_token->value;
+	parser_expected_token(parser, TOKEN_ID);
+	if (parser->cur_token->type == TOKEN_LPAREN)
+		return (parser_parse_function_call(parser));
+	ast = init_ast(AST_VARIABLE);
+	ast->variable_value = token_value;
+}
+
+t_ast		*parser_parse_variable_definition(t_parser *parser)
+{
+	t_ast	*ast;
+	t_ast	*ast_string;
+	char	*var_def_name;
+
+	parser_expected_token(parser, TOKEN_ID);
+	var_def_name = parser->cur_token->value;
+	parser_expected_token(parser, TOKEN_ID);
+	parser_expected_token(parser, TOKEN_EQUALS);
+	ast_string = parser_parse_string(parser);
+	parser_expected_token(parser, TOKEN_STRING);
+	ast = init_ast(AST_VARIABLE_DEFINITION);
+	ast->var_def_name = var_def_name;
+	ast->var_def_value = ast_string;
+	return (ast);
+}
+
+t_ast		*parser_parse_id(t_parser *parser)
+{
+	if (strcmp(parser->cur_token->value, "var") == 0)
+		return (parser_parse_variable_definition(parser));
+	else
+		return (parser_parse_variable(parser));
+}
+
 t_ast		*parser_parse_statement(t_parser *parser)
 {
 	switch (parser->cur_token->type)
 	{
 		case TOKEN_ID:
 			return (parser_parse_id(parser));
+		default:
+			parser_expected_token(parser, TOKEN_ID);
 	}
 }
 
@@ -62,25 +138,7 @@ t_ast		*parser_parse_statements(t_parser *parser)
 	return (ast);
 }
 
-t_ast		*parser_parse_string(t_parser *parser)
-{
-}
 
-t_ast		*parser_parse_variable(t_parser *parser)
-{
-}
-
-t_ast		*parser_parse_variable_definition(t_parser *parser)
-{
-}
-
-t_ast		*parser_parse_function_call(t_parser *parser)
-{
-}
-
-t_ast		*parser_parse_id(t_parser *parser)
-{
-}
 
 t_ast		*parser_parse_expr(t_parser *parser)
 {
