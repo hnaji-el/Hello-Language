8d4dd7952422ed849a2275ef875082242858346b
hnaji-el <hamidnaji981@gmail.com>
2021-04-20 16:35:42 +0000

    PARSER is ready

--- main.c
+++ main.c
@@ -1,17 +1,34 @@
 
-#include "lexer.h"
+#include "parser.h"
 
 int		main(void)
 {
-	t_lexer	*lexer;
-	t_token	*token;
+	t_lexer		*lexer;
+	t_parser	*parser;
+	t_ast		*ast;
 
 	lexer = init_lexer("var name = \"hamid\";\n""print(name);\n");
+	parser = init_parser(lexer);
+	ast = parser_parse(parser);
+	printf(
+			"%d\n%d\n%s\n",
+			ast->type,
+			ast->compound_size,
+			ast->compound_value[1]->variable_value
+			);
+	return (0);
+
+	/*
+	t_token		*token;
+	t_lexer		*lexer;
+
+	lexer = init_lexer("{var name = \"hamid\";\n""print(name);\n");
 	token = lexer_get_next_token(lexer);
 	while (token->type != TOKEN_EOF)
 	{
 		printf("TOKEN(%s, %d)\n", token->value, token->type);
 		token = lexer_get_next_token(lexer);
 	}
+	*/
 	return (0);
 }
--- parser.c
+++ parser.c
@@ -12,6 +12,12 @@ t_parser	*init_parser(t_lexer *lexer)
 	return (parser);
 }
 
+/*
+ * Expected function:
+ * die if "Unexpected token", or
+ * advance if "expacted token".
+ */
+
 void		parser_expected_token(t_parser *parser, t_token_type type)
 {
 	if (parser->cur_token->type == type)
@@ -29,37 +35,61 @@ void		parser_expected_token(t_parser *parser, t_token_type type)
 	}
 }
 
+/*
+ * Add others Data types here.
+ * integers, float, double, ...
+ */
+
+t_ast		*parser_parse_expr(t_parser *parser)
+{
+	switch (parser->cur_token->type)
+	{
+		case TOKEN_STRING:
+			return (parser_parse_string(parser));
+		case TOKEN_ID:
+			return (parser_parse_id(parser));
+		default:
+			printf(
+					"Unexpected token `%s', with type %d",
+				   	parser->cur_token->value,
+				   	parser->cur_token->type
+					);
+			exit(EXIT_FAILURE);
+	}
+}
+
 t_ast		*parser_parse_string(t_parser *parser)
 {
 	t_ast	*ast;
 
 	ast = init_ast(AST_STRING);
-	ast->string_value = parser->cur_token->value;
+	parser_expected_token(parser, TOKEN_STRING);
+	ast->string_value = parser->prev_token->value;
 	return (ast);
 }
 
 t_ast		*parser_parse_function_call(t_parser *parser)
 {
 	t_ast	*ast;
-	t_ast	*ast_argument;
+	t_ast	*ast_expr;
 
-	if (strcmp(parser->prev_token->value, "print") == 0)
-		ast = builtin_print_function(parser)
 	ast = init_ast(AST_FUNCTION_CALL);
-	ast->compound_value = (t_ast **)malloc(sizeof(t_ast *));
-	ast_statement = parser_parse_statement(parser);
-	ast->compound_value[0] = ast_statement;
-	ast->compound_size += 1;
-	while (parser->cur_token->type == TOKEN_SEMI)
+	parser_expected_token(parser, TOKEN_LPAREN);
+	ast->function_call_name = parser->cur_token->value;
+	ast->function_call_args = (t_ast **)malloc(sizeof(t_ast *));
+	ast_expr = parser_parse_expr(parser);
+	ast->function_call_args[0] = ast_expr;
+	ast->function_call_args_size += 1;
+	while (parser->cur_token->type == TOKEN_COMMA)
 	{
-		parser_expected_token(parser, TOKEN_SEMI);
-		ast->compound_size += 1;
-		ast->compound_value = realloc(
-				ast->compound_value,
-				ast->compound_size * sizeof(t_ast *)
+		parser_expected_token(parser, TOKEN_COMMA);
+		ast->function_call_args_size += 1;
+		ast->function_call_args = realloc(
+				ast->function_call_args,
+				ast->function_call_args_size * sizeof(t_ast *)
 				);
-		ast_statement = parser_parse_statement(parser);
-		ast->compound_value[ast->compound_size - 1] = ast_statement;
+		ast_expr = parser_parse_expr(parser);
+		ast->function_call_args[ast->function_call_args_size - 1] = ast_expr;
 	}
 	return (ast);
 }
@@ -67,31 +97,31 @@ t_ast		*parser_parse_function_call(t_parser *parser)
 t_ast		*parser_parse_variable(t_parser *parser)
 {
 	t_ast	*ast;
-	t_token	*token_value;
+	char	*variable_value;
 
-	token_value = parser->cur_token->value;
 	parser_expected_token(parser, TOKEN_ID);
 	if (parser->cur_token->type == TOKEN_LPAREN)
 		return (parser_parse_function_call(parser));
+	variable_value = parser->prev_token->value;
 	ast = init_ast(AST_VARIABLE);
-	ast->variable_value = token_value;
+	ast->variable_value = variable_value;
+	return (ast);
 }
 
 t_ast		*parser_parse_variable_definition(t_parser *parser)
 {
 	t_ast	*ast;
-	t_ast	*ast_string;
+	t_ast	*var_def_value;
 	char	*var_def_name;
 
 	parser_expected_token(parser, TOKEN_ID);
-	var_def_name = parser->cur_token->value;
 	parser_expected_token(parser, TOKEN_ID);
+	var_def_name = parser->prev_token->value;
 	parser_expected_token(parser, TOKEN_EQUALS);
-	ast_string = parser_parse_string(parser);
-	parser_expected_token(parser, TOKEN_STRING);
+	var_def_value = parser_parse_expr(parser);
 	ast = init_ast(AST_VARIABLE_DEFINITION);
 	ast->var_def_name = var_def_name;
-	ast->var_def_value = ast_string;
+	ast->var_def_value = var_def_value;
 	return (ast);
 }
 
@@ -110,7 +140,12 @@ t_ast		*parser_parse_statement(t_parser *parser)
 		case TOKEN_ID:
 			return (parser_parse_id(parser));
 		default:
-			parser_expected_token(parser, TOKEN_ID);
+			printf(
+					"Unexpected token `%s', with type %d",
+				   	parser->cur_token->value,
+				   	parser->cur_token->type
+					);
+			exit(EXIT_FAILURE);
 	}
 }
 
@@ -136,12 +171,6 @@ t_ast		*parser_parse_statements(t_parser *parser)
 		ast->compound_value[ast->compound_size - 1] = ast_statement;
 	}
 	return (ast);
-}
-
-
-
-t_ast		*parser_parse_expr(t_parser *parser)
-{
 }
 
 /*
